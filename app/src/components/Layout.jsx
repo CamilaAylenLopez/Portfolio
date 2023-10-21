17f47b13d454ee1272eb5e4f479a968d28415c49
CamilaAylenLopez <camilalopezort@gmail.com>
2023-10-21 16:09:49 -0300

    creaciones

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -8,12 +8,12 @@ import './Layout.css'
 
 const Layout = () => {
     return (
-        <React.Fragment>
+        <div className="layout-container">
             <Navbar expand="lg" className="bg-body-tertiary fixed-top">
                 <Container>
                     <Navbar.Brand className="espacio">Camila Aylen Lopez</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse id="basic-navbar-nav" activeKey="/home">
                         <Nav className="justify-content-end">
                             <Link to={' '} className='linkEstilo espacio'>Home</Link>
                             <Link to={'/InfoMia'} className='linkEstilo espacio'>Información mia</Link>
@@ -23,8 +23,49 @@ const Layout = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <Outlet />
-        </React.Fragment>
+            <div className="content">
+                <Outlet />
+            </div>
+            <footer className="border-top text-muted responsiveSiteFooter footer">
+                <div class="responsiveSiteFooter__contents gr-container-fluid">
+                    <div class="gr-row">
+                        <div class="gr-col gr-col-md-3 gr-col-lg-6">
+                            <div class="gr-row">
+                                <div>
+                                    <div class="responsiveSiteFooter__socialLinkWrapper horizontal">
+                                        <h5 class="responsiveSiteFooter__heading">Mis redes sociales</h5>
+                                        <div class='espacioFooter'>
+                                            <a class="responsiveSiteFooter__socialLink hjgjuf" href="https://www.facebook.com/">
+                                                <img alt="Facebook" src="https://s.gr-assets.com/assets/site_footer/footer_facebook-ea4ab848f8e86c5f5c98311bc9495a1b.svg" />
+                                            </a>
+                                            <p class='redPosicion'>Facebook</p>
+                                        </div>
+                                        <div class='espacioFooter'>
+                                        <a class="responsiveSiteFooter__socialLink hjgjuf" href="https://twitter.com/">
+                                            <img alt="Twitter" src="https://s.gr-assets.com/assets/site_footer/footer_twitter-126b3ee80481a763f7fccb06ca03053c.svg" />
+                                        </a>
+                                        <p class='redPosicion'>Twitter</p>
+                                        </div>
+                                        <div class='espacioFooter'>
+                                        <a class="responsiveSiteFooter__socialLink hjgjuf" href="https://www.instagram.com/">
+                                            <img alt="Instagram" src="https://s.gr-assets.com/assets/site_footer/footer_instagram-d59e3887020f12bcdb12e6c539579d85.svg" />
+                                        </a>
+                                        <p class='redPosicion'>Instagram</p>
+                                        </div>
+                                        <div class='espacioFooter'>
+                                        <a class="responsiveSiteFooter__socialLink hjgjuf" href="https://www.linkedin.com/">
+                                            <img alt="LinkedIn" src="https://s.gr-assets.com/assets/site_footer/footer_linkedin-5b820f4703eff965672594ef4d10e33c.svg" />
+                                        </a>
+                                        <p class='redPosicion'>Likedin</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer >
+    </div >
     )
 }
 
